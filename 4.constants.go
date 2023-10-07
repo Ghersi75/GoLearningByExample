71d3d88b55f867c519f86a71c9f4155fe1f26b95
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-06 21:42:18 -0400

    Created file to learn about constants

---
+++ 4.constants.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func Constants() {
+	fmt.Println(s)
+
+	// Declaring this as an int would give errors since it would know its casting to other values
+	// Not declaring its type allows go to infer it when it does computations such as Sin, which expects float64
+	// Automatic casting only happens if no type is declared
+	const n = 500_000_000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+}
