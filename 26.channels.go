d875bef9559a8813ac928a7541be286779321832
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-09 15:38:13 -0400

    added comments

--- 26.channels.go
+++ 26.channels.go
@@ -9,8 +9,9 @@ func Channels() {
 
 	go func() { messages <- "ping" }()
 
+	// By default channels block until all receiving vars get their data or errors are thrown, so no sleep or wait is needed here
 	msg := <- messages
 	fmt.Println(msg)
 }
-// Interestingly, if there's no goroutines here, errors come up about all goroutines being asleep - deadlock
+// Interestingly, if there's no goroutines here and you try to received data from an empty channel, errors come up about all goroutines being asleep - deadlock
 // Not sure why this is the case, but it seems like channels need goroutines to work properly, understandably
